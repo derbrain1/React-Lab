2b1cd7e7b9894266770a57dbeee6043955c478f7
Nikita <nikitachernyshev04@mail.ru>
2026-02-17 18:12:59 +0300

    2 лаба 8 семестр

--- rental-service/server/index.js
+++ rental-service/server/index.js
@@ -3,13 +3,24 @@ import * as dotenv from 'dotenv';
 import sequelize from './config/database.js';
 import cors from 'cors';
 import {router} from './routes/index.js';
+import {fileURLToPath} from 'url';
+import path from 'path';
+import errorMiddleware from './middleware/ErrorHandlingMiddleware.js';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/static', express.static(path.resolve(__dirname, 'static')));
+
 app.use('/', router);
+app.use(errorMiddleware);
 
 const start = async () => {
     try {
--- rental-service/server/routes/offerRoutes.js
+++ rental-service/server/routes/offerRoutes.js
@@ -1,7 +1,17 @@
 import {Router} from 'express';
-import { getAllOffers } from '../controllers/offerController.js';
+import { createOffer, getAllOffers } from '../controllers/offerController.js';
+import upload from '../middleware/upload.js';
+import { getFullOffer } from '../controllers/offerController.js';
+
+
 const router = new Router();
+
 router.get('/offers', getAllOffers);
+router.get('/offers/:id', getFullOffer)
+router.post('/offers', upload.fields([
+    { name: 'previewImage', maxCount: 1},
+    { name: 'photos', maxCount: 6}
+]), createOffer);
 
 
 
